48e9d217160f47c9b36cce020ee1de1ce9284874
dstrzykalski <dstrzykalski@yahoo.com>
2024-08-20 21:19:37 -0400

    fix

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ inquirer.prompt(questions).then(answers => {
 
     const svgFile = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${shape.render()}
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${text}">${textColor}</text>
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
 </svg>`
 
     fs.writeFile('image.svg', svgFile, (err) => {
