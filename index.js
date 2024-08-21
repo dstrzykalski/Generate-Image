802865c988233770229c5fe45e86c786535487df
dstrzykalski <dstrzykalski@yahoo.com>
2024-08-20 21:18:11 -0400

    Check the Text

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ inquirer.prompt(questions).then(answers => {
 
     const svgFile = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${shape.render()}
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${text}">${textColor}</text>
 </svg>`
 
     fs.writeFile('image.svg', svgFile, (err) => {
         if (err) throw err;
-        console.log('The file has been saved!');
+        console.log('Text Test!');
     });
 }
 );
